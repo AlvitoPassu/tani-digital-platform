f19fe914e6cfdad89e90e31aca1858dfe43d988f
AlvitoPassu <S22310493@student.unklab.ac.id>
2025-06-17 11:20:48 +0800

    Menambahkan fitur login user

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://kkxhaedaekxosbyzmjit.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtreGhhZWRhZWt4b3NieXptaml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MTY2MTEsImV4cCI6MjA2NTI5MjYxMX0.zwPlGGB-_lODoO_SbubdZezWFTrBGAcGi6BiFFydG28';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
